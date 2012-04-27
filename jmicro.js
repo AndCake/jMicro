f7e63165ed428bf7d0385498a7986824af4b067a
AndCake <rk@sophotec.com>
2012-04-27 22:26:01 +0200

    beautified code; improved minification results

--- jmicro.js
+++ jmicro.js
@@ -1,82 +1,97 @@
 jMicro = function () {
-	var v = "value", i = "innerHTML";
-    var a = function (element) {
+    var value = "value", innerhtml = "innerHTML", readystatechange = "readystatechange", xmlhttprequest = XMLHttpRequest, readystate = 'readyState', responsetext = "responseText", 
+	a = function (element) {
             return a.extend(typeof element == "string" && document.getElementById(element) || element, a)
         };
+
     a.elize = function (a, b) {
         var c = a;
         return function () {
             return c.apply(b, arguments)
         }
     };
+
     a.extend = function (b, c) {
         b._m = 1;
         for (var d in c) typeof c[d] == "function" && (b[d] = a.elize(c[d], b));
         return b
     };
-    a.listen = function (a, b) {
-        o = this["on" + a];
-        this["on" + a] = function (a) {
-            return (typeof o != "function" || o(a) !== false) && (b.apply(this, [a || window.event]) === false ? false : true)
+
+    a.listen = function (eventName, callback) {
+        original = this["on" + eventName];
+        this["on" + eventName] = function (event) {
+            return (typeof original != "function" || original(event) !== false) && (callback.apply(this, [event || window.event]) === false ? false : true)
         };
         return this
     };
-    a.css = function (a) {
-        this.style.cssText += ";" + a;
+
+    a.css = function (style) {
+        this.style.cssText += ";" + style;
         return this
     };
-    a.down = function (b) {
-        return a(this.querySelector(b))
+
+    a.down = function (selector) {
+        return a(this.querySelector(selector))
     };
+
     a.up = function () {
         return a(this.parentNode)
     };
+
     a.remove = function () {
         a(this).up().removeChild(this)
     };
-    a.encode = function (a) {
-        var b = encodeURIComponent,
-            c, d;
-        if (typeof a == "string") return a;
-        for (d in a) typeof a[d] != "function" && (a[d].value && (c += "&" + a[d].name + "=" + b(a[d].value)) || (c += "&" + d + "=" + b(a[d].value || a[d])));
-        return c
+
+    a.encode = function (data) {
+        var component = encodeURIComponent,
+            content, all;
+        if (typeof data == "string") return data;
+        for (all in data) 
+            typeof data[all] != "function" && (data[all][value] && (content += "&" + data[all].name + "=" + component(data[all][value])) || (content += "&" + all + "=" + component(data[all][value] || data[all])));
+        return content
     };
-    a.set = function (a) {
-        var b = this;
-        return b._m && (b[v] !== undefined && (b[v] = a) || (b[i] = a) || (b = a)) && b
+
+    a.set = function (data) {
+        var element = this;
+        return element._m && (element[value] !== undefined && (element[value] = data) || (element[innerhtml] = data) || (element = data)) && element
     };
-    a.get = function (b, callback) {
-        var d = this,
-            e;
-    	if(!b && !callback)return d[v] || d[i];
-        (e = a(new XMLHttpRequest).listen("readystatechange", function (a) {
-            e.readyState == 4 && (d.set(e.responseText) || true) && c && c(e)
-        })).open("GET", b, true);
-        e.send(null)
+
+    a.get = function (url, callback) {
+        var element = this,
+            xmlrequest;
+    	if(!url && !callback)return element[value] || element[innerhtml];
+        (xmlrequest = a(new xmlhttprequest).listen(readystatechange, function (a) {
+            xmlrequest[readystate] == 4 && (element.set(xmlrequest[responsetext]) || true) && callback && callback(xmlrequest)
+        })).open("GET", url, true);
+        xmlrequest.send(null)
     };
-    a.post = function (b, c, d) {
-        var e = this,
-            f;
-        (f = a(new XMLHttpRequest).listen("readystatechange", function (a) {
-            f.readyState == 4 && (e.set(f.responseText) || true) && d && d(f)
-        })).open("POST", b, true);
-        f.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        f.send(a.encode(c))
+
+    a.post = function (url, data, callback) {
+        var element = this,
+            xmlrequest;
+        (xmlrequest = a(new xmlhttprequest).listen(readystatechange, function (a) {
+            xmlrequest[readystate] == 4 && (element.set(xmlrequest[responsetext]) || true) && callback && callback(xmlrequest)
+        })).open("POST", url, true);
+        xmlrequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xmlrequest.send(a.encode(data))
     };
-	a.create = function(n, b) {
-		var el = document.createElement(n);
-		for(var i in b) 
-			el[i] = b[i];
-		el = a.extend(el, this);
-		return el;
-	};
-	a.add = function(c) {
-		var d = this;
-		if (typeof(c) != "string")
-			d.appendChild(c);
-		else 
-			d[i] = d[i] + c;
-		return d;
-	};    
+
+    a.create = function(tagName, attributes) {
+        var element = document.createElement(tagName),all;
+	for(all in attributes) 
+	    element[all] = attributes[all];
+	element = a.extend(element, this);
+	return element;
+    };
+    
+    a.add = function(content) {
+        var element = this;
+	if (typeof(content) != "string")
+	    element.appendChild(content);
+	else 
+	    element[innerhtml] = element[innerhtml] + content;
+	return element;
+    };    
+
     return a
 }();
